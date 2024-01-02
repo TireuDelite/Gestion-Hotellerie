0b718d059ecbbf7a9fd4918c46b487119f6519f6
TireuDelite <anthony.buffet80@gmail.com>
2024-01-02 14:25:50 +0100

    Ajout du prix ainsi que du symbole Euro (Testé sous linux et fonctionnelle)

--- Chambres.java
+++ Chambres.java
@@ -1,14 +1,16 @@
 public enum Chambres {
-    Chambre_1(12, 1),
-    Chambre_2(13, 1),
-    Chambre_3(25, 2),
-    Chambre_4(32, 2),
-    Chambre_5(46, 2),
-    Chambre_6(96, 3),
-    Chambre_7(24, 1);
+
+    Chambre_1(12, 1, 50),
+    Chambre_2(13, 1, 53),
+    Chambre_3(25, 2, 71),
+    Chambre_4(32, 2, 83),
+    Chambre_5(46, 2, 102),
+    Chambre_6(96, 3, 186),
+    Chambre_7(24, 1, 69);
 
     private int Surface;
     private int Nombre_Lits;
+    private int Prix;
 
     public int getSurface() {
         return Surface;
@@ -18,9 +20,14 @@ public enum Chambres {
         return Nombre_Lits;
     }
 
-    private Chambres (int surface, int Nombre_Lits) {
+    public int getPrix() {
+        return Prix;
+    }
+    
+    private Chambres (int Surface, int Nombre_Lits, int Prix) {
         this.Surface = Surface;
         this.Nombre_Lits = Nombre_Lits;
+        this.Prix = Prix;
     }
 
 }
--- GestionHotellerieConsole.java
+++ GestionHotellerieConsole.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class GestionHotellerieConsole {
 
     public static void main(String[] args) {
-<<<<<<< HEAD
 
         System.out.println("----------------------------------------");
         System.out.println("Bienvenue dans votre logiciel de gestion");
@@ -29,8 +28,9 @@ public class GestionHotellerieConsole {
 
                     int surfaceChambres = chambre.getSurface();
                     int nbr_lits_Chambres = chambre.getNombre_Lits();
+                    int prix_chambres = chambre.getPrix();
 
-                    System.out.println("Le numéro de chambres est "+chambre.name()+" Sa surface est de "+surfaceChambres+" puis elle a "+nbr_lits_Chambres+" lits");
+                    System.out.println("La "+ chambre.name() +" à une surface de "+ surfaceChambres +"m² puis elle a "+ nbr_lits_Chambres +" lits "+ prix_chambres +" \u20AC");
                    
                 }
                 break;
@@ -43,7 +43,7 @@ public class GestionHotellerieConsole {
                 
                 break;
             case '5':
-                
+                System.out.println("");
                 break;
             case '6':
                 System.exit(-1);
@@ -54,48 +54,4 @@ public class GestionHotellerieConsole {
         } 
         
     }
-    
-=======
-        
-        System.out.println("------------------------------------------------------");
-        System.out.println("Bienvenue dans votre logiciel de Gestion Hotelliere");
-        System.out.println("------------------------------------------------------");
-
-        int i = 0; // Boucle du menu tant que pas répondu
-
-        System.out.println("Vous souhaitez ?");
-        System.out.println("1. Afficher les détails d'une chambre");
-        System.out.println("2. Afficher les chambres et leurs disponibilités");
-        System.out.println("3. Commander un repas ?");
-        System.out.println("4. Effectuer la réservation d'une chambre");
-        System.out.println("5. Enregistrer une facture ");
-
-        Scanner reponseMenu = new Scanner(System.in);
-        String choix = reponseMenu.next();
-
-        while (i == 0) {
-
-            switch (choix.charAt(0)) {
-                case '1':
-                    // Mettre ici le code pour détails des chambre
-                    break;
-                case '2':
-                    // Mettre ici l'affichage des chambres et de leur états
-                    break;
-                case '3':
-                    // Mettre ici le code pour la commande de repas
-                    break;
-                case '4':
-                    // Mettre ici le code pour réservation d'une chambre
-                    break;
-                case '5':
-                    // Mettre ici le code pour l'enregistrement de facture
-                    break;
-                default:
-                    System.out.println("Veuillez entrer un choix valide [1, 2, 3, 4, 5]");
-                    break;
-            }
-        }
-    }
->>>>>>> 5c520e0b37195cfc740a0d0dafd0c4f0a9e8ae92
 }
