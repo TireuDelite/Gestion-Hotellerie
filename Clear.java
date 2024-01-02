cb4dab5cffa5181314187a4b9757917a20f525da
HASHAJETER <jobontem@gmail.com>
2024-01-02 20:55:59 +0100

    ajout d'une boucle pour pouvoir faire plusieurs choix, modification de l'emplacement du Scanner scan en dehors de la boucle sinon le scaner se ferme et NOK, ajout du fichier Clear.java pour avoir une méthode Clear.clear() pour clear le cli

---
+++ Clear.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Clear {
+    public final static void clear()  
+    {  
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else
+                Runtime.getRuntime().exec("clear");
+        } catch (IOException | InterruptedException ex) {}
+    }  
+}
